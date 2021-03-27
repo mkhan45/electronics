9b34dcc12ec555d7368e42e0bb94ca4b9037a04f
Mikail Khan <mikail.khan45@gmail.com>
2021-03-27 01:26:44 -0400

    renamed web folder to docs for gh pages

--- build-web.sh
+++ build-web.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 cargo build --release --target wasm32-unknown-unknown
-cp target/wasm32-unknown-unknown/release/flappy-bird.wasm web
+cp target/wasm32-unknown-unknown/release/flappy-bird.wasm docs
 
 if [ "$1" = "serve" ]
 then
     # cargo install basic-http-server
-    basic-http-server web
+    basic-http-server docs
 fi
