2d8e916a726cf48c8689ef36d3c0015369653a31
Mikail Khan <mikail.khan45@gmail.com>
2021-04-05 13:45:37 -0400

    added wasm-strip to build-web.sh

--- build-web.sh
+++ build-web.sh
@@ -2,6 +2,9 @@
 
 cargo build --release --target wasm32-unknown-unknown
 cp target/wasm32-unknown-unknown/release/simple_electronics.wasm docs
+
+# https://github.com/WebAssembly/wabt
+wasm-strip docs/simple_electronics.wasm
 cp -r resources docs/
 
 if [ "$1" = "serve" ]
